3128a22604fee29592b609cc8b23d3104240813e
Dave <dave.cadelina@outlook.com>
2020-12-14 23:32:57 +1100

    added games list

---
+++ js/app.js
@@ -0,0 +1,13 @@
+
+const gameList = [
+    'World Of Warcraft',
+    'Overwatch',
+    'Final Fantasy 8',
+    'Dota',
+    'Crash Bandicoot',
+    'Guild Wars 2',
+    'Shadow of Mordor',
+    'Heros of the Storm',
+    'Blade & Soul',
+    'Tekken 3'
+]
